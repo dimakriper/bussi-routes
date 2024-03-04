39d14f7595d2412cac0eb7d27842d5e308c6ceb4
= <=>
2024-03-04 11:23:44 +0300

    remove route and stops on deselect

--- src/leaflet-map/bussi-routes-map.js
+++ src/leaflet-map/bussi-routes-map.js
@@ -11,7 +11,8 @@ export class BusStopMap extends BaseMap{
         super();
         this.stopMarkersLG = L.layerGroup()
         this.routesLG = L.layerGroup()
-        this.activeRoute = []
+        this.Stops = [];
+        this.Routes = [];
     }
     init(el) {
         super.init(el);
@@ -21,15 +22,25 @@ export class BusStopMap extends BaseMap{
         eventBus.$on('route-selected-from-table', (id)=> {
             this.onRouteSelected(id)
         });
-        // eventBus.$off('route-deselected-from-table');
-        // eventBus.$on('route-deselected-from-table', (id)=> {
-        //     this.clearAll()
+        eventBus.$off('route-deselected-from-table');
+        eventBus.$on('route-deselected-from-table', (id)=> {
+            this.removeRoutes([id]);
+            this.removeStops([id]);
+        });
+        // eventBus.$off('show-all-stops');
+        // eventBus.$on('show-all-stops', ()=> {
+        //     this.showAllStops();
+        // });
+        // eventBus.$off('pan-to-stop');
+        // eventBus.$on('pan-to-stop', (id)=> {
+        //     this.panToStop(id);
         // });
     }
     onRouteSelected(id){
-        this.clearAll();
+        // this.clearLayers();
 
         const route = store.getters.routeById(id);
+        let currentRoute = {route: route, layers: {forward: null, backward: null}}
 
         //creating stop markers
         const routeStops = route.Stops;
@@ -37,6 +48,7 @@ export class BusStopMap extends BaseMap{
         routeStops.forEach(pt => {
             const stopMarker = busStops.createStopMarker(pt);
             stopMarker.addTo(this.stopMarkersLG);
+            this.Stops.push({stop: pt, marker: stopMarker})
         })
 
         //creating route polylines
@@ -48,6 +60,7 @@ export class BusStopMap extends BaseMap{
             routeForward.bindTooltip(route.Description, {sticky: true})
             routeForward.addTo(this.routesLG)
             this.map.fitBounds(routeForward.getBounds())
+            currentRoute.layers.forward = routeForward
         }
 
         if (latlons.backward.length > 0){
@@ -55,10 +68,55 @@ export class BusStopMap extends BaseMap{
             routeBackward.bindTooltip(route.Description, {sticky: true})
             routeBackward.addTo(this.routesLG)
             this.map.fitBounds(routeBackward.getBounds())
+            currentRoute.layers.backward = routeBackward
         }
+        this.Routes.push(currentRoute);
+
     }
-    clearAll(){
+    clearLayers(){
         this.routesLG.clearLayers();
         this.stopMarkersLG.clearLayers();
+    }
+    removeRoutes(ids=[]){
+        let rmIndexes = []
+        if (ids.length > 0){
+            this.Routes.forEach(rt => {
+                if (ids.includes(rt.route.ID)){
+                    rt.layers.forward?.removeFrom(this.routesLG);
+                    rt.layers.backward?.removeFrom(this.routesLG);
+                    rmIndexes.push(this.Routes.indexOf(rt))
+                }
+            })
+        }
+        else {
+            this.Routes.forEach(rt => {
+                rt.layers.forward?.removeFrom(this.routesLG);
+                rt.layers.backward?.removeFrom(this.routesLG);
+                rmIndexes.push(this.Routes.indexOf(rt))
+            })
+        }
+        rmIndexes.forEach(index => {
+            this.Routes.splice(index, 1);
+        })
+
+    }
+    removeStops(routeIds=[]){
+        let rmIndexes = []
+        if (routeIds.length > 0){
+            this.Stops.forEach(pt => {
+                if (routeIds.includes(pt.stop.routeID)){
+                    pt.marker.removeFrom(this.stopMarkersLG);
+                    rmIndexes.push(this.Stops.indexOf(pt))
+                }
+            })
+        }
+        this.Stops.forEach(pt => {
+            pt.marker.removeFrom(this.stopMarkersLG);
+            rmIndexes.push(this.Stops.indexOf(pt))
+        })
+        rmIndexes.forEach(index => {
+            this.Stops.splice(index, 1);
+        })
+
     }
 }
